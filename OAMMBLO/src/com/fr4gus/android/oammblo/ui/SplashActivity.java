373a0f7f55779488d2c042567378b441cf27d8de
Gregory Obando <greobando25@hotmail.com>
2012-08-19 01:28:42 -0600

    Tarea 1. Primera sincronización.

--- OAMMBLO/src/com/fr4gus/android/oammblo/ui/SplashActivity.java
+++ OAMMBLO/src/com/fr4gus/android/oammblo/ui/SplashActivity.java
@@ -2,7 +2,10 @@ package com.fr4gus.android.oammblo.ui;
 
 import com.fr4gus.android.oammblo.R;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 /**
  * Shows App logo for few seconds.
@@ -18,4 +21,26 @@ public class SplashActivity extends OammbloActivity {
         setContentView(R.layout.splash);
     }
 
+	@Override
+	protected void onResume() {
+		// TODO Auto-generated method stub
+		super.onResume();
+        /* Trozo de codigo que se encarga de iniciar la actividad login usando el metodo postDelayed
+         * el cual ejecuta una acción luego de un determinado periodo de tiempo. Al final el
+         * splashActivity es finalizado para eviatr que cuando se use el boton bach hasta home vuelva a aparecer login
+         * Hecho en parte durante el laboratorio.
+         */
+		 Handler handler = new Handler();
+		 handler.postDelayed(new Runnable() {
+
+			 public void run(){
+				
+				 startActivity(new Intent(getApplicationContext(), Login_Activity.class));
+				 finish();
+			 }
+		 }, 3000);
+		 
+
+		
+	}
 }
